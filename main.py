49c4d9e8595ce0af8983ac7a4eb01a487e269aad
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-08-04 00:13:15 +0900

    Update main.py
    ルーム一覧取得を追加

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import requests
-import json
 import settings
 
-API_URL = "https://api.chatwork.com/v2"
+API_URL = "https://api.chatwork.com/v2/"
+API_KEY = settings.getenv("API_KEY")
+CHAT_WORK_TOKEN = {'X-ChatWorkToken': API_KEY}
 
-print(settings.getenv("API_KEY"))
+
+def get_rooms():
+    """get_rooms
+    ルーム一覧を取得する
+    返却値はAPIドキュメント参照(http://developer.chatwork.com/ja/endpoint_rooms.html)
+    """
+
+    ret = requests.get(API_URL + 'rooms', headers=CHAT_WORK_TOKEN)
+    return ret.json()
+
+
+def main():
+    get_rooms()
+
+
+if __name__ == '__main__':
+    main()
